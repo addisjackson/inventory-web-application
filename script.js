f70cc4c8a55b42582f441ad653b871ac8faa15d2
addisjackson <addisjackson@pursuit.org>
2023-07-07 19:01:43 -0400

    finishes project

--- script.js
+++ script.js
@@ -1,71 +1,63 @@
-const selectedRow = null;
+var selectedRow = null
 
 function onFormSubmit(e) {
-    const formData = readFormData();
-    if (selectedRow == null) {
-        insertNewRecord(formData);
-    } else {
-        updateRecord(formData);
-    }
-    resetForm();
+	event.preventDefault();
+        var formData = readFormData();
+        if (selectedRow == null){
+            insertNewRecord(formData);
+		} else {
+            updateRecord(formData);
+		}
+        resetForm();    
 }
 
-//Get data from form
 
+
+//Retrieve the data
 function readFormData() {
-    const flavorDropDown = document.getElementById("flavor");
-    const selectedFlavor = flavorDropDown.options[flavorDropDown.selectedIndex].value;
-    const formData = {};
-    formData["date"] = document.getElementById('date').value;
-    if (selectedFlavor == "Flavor not listed") {
-        formData["flavor"] = document.getElementById('newFlavorInput').value;
-    } else {
-        formData["flavor"] = selectedFlavor;
-    }
-    formData["purchasePrice"] = document.getElementById('purchasePrice').value;
-    formData["quantityPurchased"] = document.getElementById('quantityPurchased').value;
-    formData["quantityRemaining"] = document.getElementById('quantityRemaining').value;
-    if (parseInt(formData['quantityRemaining'])< 5) {
-        formData["order"] = document.getElementById('order').checked.value="yes";
-    } else {
-        formData["order"] = document.getElementById('order').checked.value="no";
-    }
+
+  var formData = {};
+    formData["date"] = document.getElementById("date").value;
+    formData["flavor"] = document.getElementById("flavor").value;
+    formData["productPrice"] = document.getElementById("productPrice").value;
+    formData["quantityPurchased"] = document.getElementById("quantityPurchased").value;
+    formData["quantityRemaining"] = document.getElementById("quantityRemaining").value;
+    formData["order"] = document.getElementById("order").checked.value="yes";
     return formData;
 }
 
-//add data to table
-
+//Insert the data
 function insertNewRecord(data) {
-    const table = document.getElementById("storelist").getElementsByTagName("tbody")[0];
-    const newRow = table.insertRow(table.length);
-    cell1 =newRow.insertCell(0);
-        cell1.innerHTML = data.date;
-    cell2 =newRow.insertCell(1);
-        cell2.innerHTML = data.flavor;
-    cell3 =newRow.insertCell(2);
-        cell3.innerHTML = data.purchasePrice;
-    cell4 =newRow.insertCell(3);
-        cell4.innerHTML = data.quantityPurchased;
-    cell5 =newRow.insertCell(4);
-        cell5.innerHTML = data.quantityRemaining;
-    cell6 =newRow.insertCell(5);
-        cell6.innerHTML = data.order;
-    cell7 =newRow.insertCell(6);
+    var table = document.getElementById("storeList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+		cell1.innerHTML = data.date;
+    cell2 = newRow.insertCell(1);
+		cell2.innerHTML = data.flavor;
+    cell3 = newRow.insertCell(2);
+		cell3.innerHTML = data.productPrice;
+    cell4 = newRow.insertCell(3);
+		cell4.innerHTML = data.quantityPurchased;
+    cell5 = newRow.insertCell(4);
+		cell5.innerHTML = data.quantityRemaining;
+    cell6 = newRow.insertCell(5);
+		cell6.innerHTML = data.order;
+    cell7 = newRow.insertCell(6);
         cell7.innerHTML = `<button onClick="onEdit(this)">Edit</button> <button onClick="onDelete(this)">Delete</button>`;
 }
 
-//editing data in table
-
+//Edit the data
 function onEdit(td) {
     selectedRow = td.parentElement.parentElement;
     document.getElementById("date").value = selectedRow.cells[0].innerHTML;
     document.getElementById("flavor").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("purchasePrice").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("productPrice").value = selectedRow.cells[2].innerHTML;
     document.getElementById("quantityPurchased").value = selectedRow.cells[3].innerHTML;
     document.getElementById("quantityRemaining").value = selectedRow.cells[4].innerHTML;
     document.getElementById("order").value = selectedRow.cells[5].innerHTML;
-}
 
+
+}
 function updateRecord(formData) {
     selectedRow.cells[0].innerHTML = formData.date;
     selectedRow.cells[1].innerHTML = formData.flavor;
@@ -75,24 +67,22 @@ function updateRecord(formData) {
     selectedRow.cells[5].innerHTML = formData.order;
 }
 
-//Deleting data from the table
-
+//Delete the data
 function onDelete(td) {
-    if(confirm("Do you want to delete this record?")) {
+    if (confirm('Do you want to delete this record?')) {
         row = td.parentElement.parentElement;
-        document.getElementById("storelist").deleteRow(row.rowIndex);
+        document.getElementById('storeList').deleteRow(row.rowIndex);
         resetForm();
     }
 }
 
-//resetting the form
- 
+//Reset the data
 function resetForm() {
-    document.getElementById("date").value = " ";
-    document.getElementById("flavor").value = " ";
-    document.getElementById("purchasePrice").value = " ";
-    document.getElementById("quantityPurchased").value = " ";
-    document.getElementById("quantityRemaining").value = " ";
-    document.getElementById("order").value = " ";
+    document.getElementById("date").value = '';
+    document.getElementById("flavor").value = '';
+    document.getElementById("productPrice").value = '';
+    document.getElementById("quantityPurchased").value = '';
+    document.getElementById("quantityRemaining").value = '';
+    document.getElementById("order").value = '';
     selectedRow = null;
 }
